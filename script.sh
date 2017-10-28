69db5dd024745382f6b6a829d2e7da1212ab94ec
GhaithCraft <GhaithCraft@Gmail.com>
2017-10-28 18:03:06 +0300

    Stock A720FXXU2BQH3

---
+++ script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#Cleanup before build
+
+echo "Compile wtih linaro Toolchain"
+export CROSS_COMPILE=/home/ghaith/android/Toolchains/gcc-linaro-7.1.1-2017.08-i686_aarch64-linux-gnu/bin/aarch64-linux-gnu-
+ARCH=arm64
+
+echo "CleanUp"
+make clean && make mrproper
+
+echo "Stock DEFCON"
+make Alphabet_Kernel_a7y17lte_defconfig
+#make Alphabet_Kernel_a5y17lte_defconfig
+make -j3
---
+++ upload.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#start
+git init
+git add .
+git commit -m "Stock A720FXXU2BQH3"
+git remote add origin https://github.com/GhaithCraft/Alphabet-Kernel-A5-A7-2017.git
+git push -u origin master
